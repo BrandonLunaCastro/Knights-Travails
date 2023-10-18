3aa35f9df457f5f5ae3986b72a459cbfb969c2c3
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-10-18 10:10:42 -0300

    feat: add function gameBoard to generate a chess board

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const gameBoard = () => {
   const board = [];
   for (let w = 0; w < 8; w++) {
+    const fila = [];
     for (let h = 0; h < 8; h++) {
-      board.push([w][h]);
+      fila.push(h);
     }
+    board.push(fila);
   }
+  board[3][3] = "knight";
   console.log(board);
 };
 console.log(gameBoard());

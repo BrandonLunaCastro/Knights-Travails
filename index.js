e6c55bc472b27a26791666b78845c9c9bdc78f66
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-10-18 11:48:57 -0300

    fix: refactor function gameBoard

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const gameBoard = () => {
-  const board = [];
-  for (let w = 0; w < 8; w++) {
-    const fila = [];
-    for (let h = 0; h < 8; h++) {
-      fila.push(h);
+  const board = new Map();
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      const box = `${row}-${col}`;
+      const validMovements = [];
+      board.set(box, validMovements);
     }
-    board.push(fila);
   }
-  board[3][3] = "knight";
   console.log(board);
 };
 console.log(gameBoard());

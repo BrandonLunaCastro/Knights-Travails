c2dde020e292490defc7ef3b7500e83406ad3299
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-10-19 17:57:59 -0300

    feat: add some comments

--- modules/knightMoves.js
+++ modules/knightMoves.js
@@ -9,12 +9,15 @@ const knightMoves = (start, end) => {
   const board = gameBoard(rows, cols);
   const init = start[0];
   const final = start[1];
+  // mark out board the position start
   board[init][final] = true;
   const queue = [];
   const movements = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [-1, 2], [1, -2], [-1, -2]];
+  // push in the queue first element with the position and route equal to start parameter
   queue.push({ position: start, route: [start] });
 
   while (queue.length > 0) {
+    // destructure the first element on the enqueue in position and route
     const { position, route } = queue.shift();
     if (position[0] === end[0] && position[1] === end[1]) {
       return { movements: route.length - 1, route };

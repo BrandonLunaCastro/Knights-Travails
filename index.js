09672cc9f2386e58f2d2ecad7453a9f3cb0092a6
BrandonLunaCastro <blunacastro2003@gmail.com>
2023-10-19 18:00:32 -0300

    feat: show the shortest path

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import knightMoves from "./modules/knightMoves.js";
 const moves = knightMoves([0, 0], [3, 3]);
-if (moves.route) {
+const route = moves.route;
+if (route) {
   console.log(`You made it in ${moves.movements} moves!  Here's your path:`);
   for (const position of moves.route) {
     console.log(position);
